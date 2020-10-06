2450016b3945fad0e77b376a16b8d19ca9f63504
YavorskiE <YavorskiE@outlook.com>
2020-10-06 00:24:24 -0400

    1.5. Поля объекта [#326794]

--- src/main/java/ru/jo4j/oop/Cat.java
+++ src/main/java/ru/jo4j/oop/Cat.java
@@ -2,15 +2,40 @@ package ru.jo4j.oop;
 
 public class Cat {
 
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void show() {
+        System.out.println(this.name + " eats " + this.food);
+    }
+
     public String sound() {
         String voice = "may-may";
         return voice;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
+//        Cat peppy = new Cat();
 //        Cat sparky = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+//        String say = peppy.sound();
+//        System.out.println("Peppy says " + say);
+        System.out.println("These are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("These are black's food.");
+        Cat black = new Cat();
+        black.giveNick("black");
+        black.eat("fish");
+        black.show();
     }
 }
