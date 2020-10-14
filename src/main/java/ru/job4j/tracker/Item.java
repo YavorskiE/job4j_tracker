356606a4eb329d140870224bf737ce771ce9eb41
YavorskiE <YavorskiE@outlook.com>
2020-10-14 02:56:10 -0400

    2.1. Реализация класса StartUI [#326770]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,12 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {}
+
+    public Item(String name) {
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
@@ -18,5 +24,13 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
