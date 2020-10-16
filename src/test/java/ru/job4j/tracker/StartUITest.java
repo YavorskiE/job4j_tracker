cbacb8fa9a3111492ab02620d02c15d70b6d37a9
YavorskiE <YavorskiE@outlook.com>
2020-10-16 00:40:36 -0400

    5.1. Тестирование. Подготовка данных. [#326767]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,11 +1,11 @@
 package ru.job4j.tracker;
 
-import org.hamcrest.Matcher;
 import org.junit.Test;
 
 import static java.lang.String.valueOf;
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.nullValue;
 
 public class StartUITest {
 
@@ -38,7 +38,9 @@ public class StartUITest {
         tracker.add(item);
         String[] answers = {valueOf(item.getId())};
         StartUI.deleteItem(new StubInput(answers), tracker);
-        Item[] deleted = tracker.findByName(item.getName());
-        assertThat(deleted.length, is(0));
+//        Item[] deleted = tracker.findByName(item.getName());
+//        assertThat(deleted.length, is(0));
+        Item deleted = tracker.findById(item.getId());
+        assertThat(deleted, is(nullValue()));
     }
 }
