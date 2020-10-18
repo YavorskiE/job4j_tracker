007d0f02093683040e46eb1979e149c755468fa7
YavorskiE <YavorskiE@outlook.com>
2020-10-18 13:58:55 -0400

    9.2. Тесты вывода на консоль [#326757]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -9,7 +9,7 @@ public class StartUITest {
 
     @Test
     public void whenAddItem() {
-        Output out = new ConsoleOutput();
+        Output out = new StubOutput();
         Input in = new StubInput(
                 new String[] {"0", "Item number", "1"}
         );
@@ -24,7 +24,7 @@ public class StartUITest {
 
     @Test
     public void whenReplaceItem() {
-        Output out = new ConsoleOutput();
+        Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Replaced item"));
         String replacedName = "New item name";
@@ -41,7 +41,7 @@ public class StartUITest {
 
     @Test
     public void whenDeleteItem() {
-        Output out = new ConsoleOutput();
+        Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = new Item("Deleted item");
         tracker.add(item);
@@ -58,7 +58,7 @@ public class StartUITest {
 
     @Test
     public void whenFindAll() {
-        Output out = new ConsoleOutput();
+        Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = new Item("First item");
         tracker.add(item);
@@ -70,8 +70,51 @@ public class StartUITest {
                 new Exit()
         };
         new StartUI(out).init(in, tracker, actions);
-        String actual = out.toString();
-        String expected = item.getName();;
+        String message = out.toString();
+        String actual = message.split("\r\n")[4];
+        String expected = "Item{id=" + item.getId() + ", name='" + item.getName() + "'}";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenFindId() {
+        Tracker tracker = new Tracker();
+        Item[] items = {new Item("First item"), new Item("Second Item")};
+        tracker.add(items[0]);
+        tracker.add(items[1]);
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0","2","1"}
+        );
+        UserAction[] actions = {
+                new FindIdAction(out),
+                new Exit()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String message = out.toString();
+        String actual = message.split("\r\n")[4];
+        String expected = "Item{id=" + items[1].getId() + ", name='" + items[1].getName() + "'}";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenFindName() {
+        Tracker tracker = new Tracker();
+        Item[] items = {new Item("First item"), new Item("Second item")};
+        tracker.add(items[0]);
+        tracker.add(items[1]);
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0","Second item","1"}
+        );
+        UserAction[] actions = {
+                new FindNameAction(out),
+                new Exit()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String message = out.toString();
+        String actual = message.split("\r\n")[4];
+        String expected = "Item{id=" + items[1].getId() + ", name='" + items[1].getName() + "'}";
         assertThat(actual, is(expected));
     }
 }
