f0649c21e715fdbef9af980eb666b63c3ffeb6e0
YavorskiE <YavorskiE@outlook.com>
2020-10-18 16:43:58 -0400

    0.2. Кидаем исключение - throw new RuntimeException [#326778]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,23 @@
+package ru.job4j.ex;
+
+import java.util.Scanner;
+
+public class Fact {
+
+    public static void main(String[] args) {
+        System.out.print("Enter your number : ");
+        int n = Integer.parseInt(new Scanner(System.in).nextLine());
+        System.out.println(calc(n));
+    }
+
+    public static int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Argument cannot be negative");
+        }
+        int rsl = 1;
+        for(int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
