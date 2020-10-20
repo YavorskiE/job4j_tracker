8260807dec7951dc312eccf7c0bd46bda69ef602
YavorskiE <YavorskiE@outlook.com>
2020-10-20 02:15:36 -0400

    0.4. Пользовательские исключения. [#326780]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,9 +4,6 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int index = 0; index < value.length; index++) {
-            if (value[index] == null) {
-                throw new NullPointerException("Element " + index + " refers null pointer");
-            }
             if (key.equals(value[index])) {
                 rsl = index;
                 break;
