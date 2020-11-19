4d73c914e25bbe45be9298e20fc77f9dd93da9b0
YavorskiE <YavorskiE@outlook.com>
2020-11-19 01:07:59 -0500

    2. Идентичные тексты [#326751]

---
+++ src/test/java/ru/job4j/collection/UniqueTextTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class UniqueTextTest {
+
+    @Test
+    public void isEqual() {
+        String origin = "My cat eats a mouse and milk";
+        String text = "My cat eats milk and a mouse";
+        assertThat(UniqueText.isEqual(origin, text), is(true));
+    }
+    @Test
+    public void isNotEqual() {
+        String origin = "My cat eats a mouse";
+        String text = "A mouse is eaten by my cat";
+        assertThat(UniqueText.isEqual(origin, text), is(false));
+    }
+
+}
