0b11f48171e75c0ca55a14f938ee6e09893ae001
YavorskiE <YavorskiE@outlook.com>
2020-11-23 02:33:26 -0500

    1. Паспорт и жители [#326746], исправлен

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -15,9 +15,6 @@ public class PassportOffice {
     }
 
     public Citizen get(String passport) {
-        if (citizens.containsKey(passport)) {
-            return citizens.get(passport);
-        }
-        return null;
+        return citizens.get(passport);
     }
 }
