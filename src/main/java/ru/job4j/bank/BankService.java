f776d98950356a240648dd8525768edef74f4e27
YavorskiE <YavorskiE@outlook.com>
2020-11-24 04:35:46 -0500

    3. Банковские переводы [#326747]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,55 @@
+package ru.job4j.bank;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User  user) {
+        users.putIfAbsent(user, new ArrayList<>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            if (!accounts.contains(account)) accounts.add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) return user;
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if(user != null) {
+            List<Account> accounts = users.get(user);
+//            for (Account account : accounts) {
+//                if (account.getRequisite().equals(requisite)) return account;
+//            }
+            int index = accounts.indexOf(new Account(requisite, 0));
+            if (index != -1) return accounts.get(index);
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                            String destPassport, String destRequisite, double amount) {
+        boolean rsl =  false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && destAccount != null && amount <= srcAccount.getBalance()) {
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
