934819540f980ea434dee40bcbeb44d1a77f3374
YavorskiE <YavorskiE@outlook.com>
2020-12-14 02:34:45 -0500

    Проба 3

--- src/main/java/ru/job4j/lambda/FI.java
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -10,15 +10,59 @@ public class FI {
                 new Attachment("Image 3", 120),
                 new Attachment("Image 2", 23)
         };
-        Comparator<Attachment> comparator = new Comparator<>() {
-            @Override
-            public int compare(Attachment o1, Attachment o2) {
-                return o1.getSize() - o2.getSize();
-            }
-        };
+        Comparator<Attachment> comparator = (left, right) -> left.getSize() - right.getSize();
         Arrays.sort(atts, comparator);
         for (Attachment att : atts) {
             System.out.println(att);
+        }
+
+        Comparator<String> cmpText = (left, right) -> left.compareTo(right);
+
+        Comparator<String> cmpDescSize = (left, right) -> right.length() - left.length();
+
+        String[] array = {"First", "Second", "Third", "Fourth"};
+        for (String s : array) {
+            System.out.println(s);
+        }
+        System.out.println();
+
+        Arrays.sort(array);
+        for (String s : array) {
+            System.out.println(s);
+        }
+        System.out.println();
+
+        Arrays.sort(array, cmpText);
+        for (String s : array) {
+            System.out.println(s);
+        }
+        System.out.println();
+
+        Arrays.sort(array, cmpDescSize);
+        for (String s : array) {
+            System.out.println(s);
+        }
+
+        String[] numbers = {
+                "1. Task",
+                "2. Task",
+                "11. Task"
+        };
+
+        Comparator<String> numStr = (left, right) -> {
+            int first = Integer.valueOf(left.substring(0, left.indexOf('.')));
+            int second = Integer.valueOf(right.substring(0, right.indexOf('.')));
+            return first - second;
+        };
+
+        for (String s : numbers) {
+            System.out.println(s.substring(0, s.indexOf('.')));
+        }
+        System.out.println();
+
+        Arrays.sort(numbers, numStr);
+        for (String s : numbers) {
+            System.out.println(s);
         }
     }
 }
