32021d85d90dcf603d815f5df0fb7c50cbbbb15f
YavorskiE <YavorskiE@outlook.com>
2020-12-17 01:12:32 -0500

    0. Stream API [#326584]

---
+++ src/main/java/ru/job4j/stream/IntegerStream.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class IntegerStream {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(2, 0, -44, 777, 1, -1, 12, 1, -3);
+        List<Integer> filtered = list.stream()
+                .filter(num -> num >= 0)
+                .collect(Collectors.toList());
+        filtered.forEach(System.out::println);
+    }
+}
