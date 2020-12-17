dfb84508c63fa6e3e09c4fa60d24d1e146ee0dcd
YavorskiE <YavorskiE@outlook.com>
2020-12-17 02:15:11 -0500

    0. Stream API [#326584], исправлен

--- src/main/java/ru/job4j/stream/IntegerStream.java
+++ src/main/java/ru/job4j/stream/IntegerStream.java
@@ -8,7 +8,7 @@ public class IntegerStream {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(2, 0, -44, 777, 1, -1, 12, 1, -3);
         List<Integer> filtered = list.stream()
-                .filter(num -> num >= 0)
+                .filter(num -> num > 0)
                 .collect(Collectors.toList());
         filtered.forEach(System.out::println);
     }
