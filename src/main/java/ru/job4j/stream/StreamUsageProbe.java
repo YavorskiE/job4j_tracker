6da1b0d3385ab46694691aa1a2be5c0a1f3e3945
YavorskiE <YavorskiE@outlook.com>
2020-12-17 01:28:00 -0500

    Rename to Probe 1

--- src/main/java/ru/job4j/stream/StreamUsageProbe.java
+++ src/main/java/ru/job4j/stream/StreamUsageProbe.java
@@ -4,11 +4,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamUsageProbe {
-    public static class Task {
+    public static class Task0 {
         private final String name;
         private final long spent;
 
-        public Task(String name, long spent) {
+        public Task0(String name, long spent) {
             this.name = name;
             this.spent = spent;
         }
@@ -23,21 +23,21 @@ public class StreamUsageProbe {
     }
 
     public static void main(String[] args) {
-        List<Task> tasks = List.of(
-                new Task("Bug #1",  100),
-                new Task("Task #2", 100),
-                new Task("Bug #3", 100)
+        List<Task0> task0s = List.of(
+                new Task0("Bug #1",  100),
+                new Task0("Task #2", 100),
+                new Task0("Bug #3", 100)
         );
-        List<Task> bugs = tasks.stream()
-                .filter(task -> task.name.contains("Bug"))
+        List<Task0> bugs = task0s.stream()
+                .filter(task0 -> task0.name.contains("Bug"))
                 .collect(Collectors.toList());
         bugs.forEach(System.out::println);
-        List<String> names = tasks.stream()
-                .map(task -> task.name)
+        List<String> names = task0s.stream()
+                .map(task0 -> task0.name)
                 .collect(Collectors.toList());
         names.forEach(System.out::println);
-        long total = tasks.stream()
-                .map(task -> task.spent)
+        long total = task0s.stream()
+                .map(task0 -> task0.spent)
                 .reduce(0L, Long::sum);
         System.out.println(total);
     }
