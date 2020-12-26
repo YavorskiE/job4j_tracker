e1465f0cf63c2cebaf9431570ff3fc8eef32dbf2
YavorskiE <YavorskiE@outlook.com>
2020-12-25 20:46:22 -0500

    0. Stream Play

--- src/main/java/ru/job4j/stream/StreamPlay.java
+++ src/main/java/ru/job4j/stream/StreamPlay.java
@@ -1,29 +1,14 @@
 package ru.job4j.stream;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Map;
+import java.util.Set;
 
 public class StreamPlay {
-    public List<Student> levelOf(List<Student> students, int bound) {
-        return students.stream()
-                .flatMap(Stream::ofNullable)
-                .sorted()
-                .filter(student -> student.getScore() > bound)
-                .collect(Collectors.toList());
-//        Function<Student, Stream<Student>> function = new Function<Student, Stream<Student>>() {
-//            @Override
-//            public Stream<Student> apply(Student student) {
-//                return Stream.ofNullable(student);
-//            }
-//        }
-//        Stream<Student> stdStream = students.stream();
-//        Stream<Student> nulStream = stdStream.flatMap(Stream::ofNullable);
-//        Stream<Student> nulStream = stdStream.flatMap(s -> Stream.ofNullable(s));
-//        Stream<Student> nulStream = stdStream.flatMap(function);
-//        Stream<Student> sortedStream = nulStream.sorted();
-//        Stream<Student> filteredStream = sortedStream.filter(s -> s.getScore() >= bound);
-//        List<Student> rsl = filteredStream.collect(Collectors.toList());
-//        return rsl;
+    public static void main(String[] args) {
+        List.of(5, 1, 2).forEach(System.out::print);
+        Set.of(5, 1, 2).forEach(System.out::print);
+        Map.of("first", 1, "second", 2)
+                .forEach((v, k) -> System.out.printf("%s %s", v, k));
     }
 }
