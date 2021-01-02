b48d176a94dc17ee7bfce99afcd8518e227da3f5
YavorskiE <YavorskiE@outlook.com>
2021-01-02 03:38:38 -0500

    2. Крестики-нолики [#326602], исправлен

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -2,7 +2,6 @@ package ru.job4j.stream;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -29,11 +28,10 @@ public class Analyze {
     }
 
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
-        Map<String, Double> subjects = stream
-                .flatMap(p -> p.getSubjects().stream())
+        return stream.flatMap(p -> p.getSubjects().stream())
                 .collect(Collectors.groupingBy(Subject::getName,
-                        Collectors.averagingDouble(Subject::getScore)));
-        return subjects.entrySet()
+                        Collectors.averagingDouble(Subject::getScore)))
+                .entrySet()
                 .stream()
                 .map(s -> new Tuple(s.getKey(), s.getValue()))
                 .collect(Collectors.toList());
@@ -53,11 +51,10 @@ public class Analyze {
     }
 
     public static Tuple bestSubject(Stream<Pupil> stream) {
-        Map<String, Double> subjects = stream
-                .flatMap(p -> p.getSubjects().stream())
+        return stream.flatMap(p -> p.getSubjects().stream())
                 .collect(Collectors.groupingBy(Subject::getName,
-                        Collectors.summingDouble(Subject::getScore)));
-        return subjects.entrySet()
+                        Collectors.summingDouble(Subject::getScore)))
+                .entrySet()
                 .stream()
                 .map(s -> new Tuple(s.getKey(), s.getValue()))
                 .max(Comparator.comparingDouble(Tuple::getScore))
